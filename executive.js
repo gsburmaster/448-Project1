e92c5c796b0b4301e9078f0413c7d50c6a8e1f9c
bcparrish <89991658+bcparrish@users.noreply.github.com>
2021-09-16 23:19:24 -0500

    Update executive.js

--- executive.js
+++ executive.js
@@ -2,7 +2,7 @@
 
 //Basic game data
 //Note other 2 arrays for tracking hits and misses havent been added
-//Planned on 1 meaning ship is at spot, 2 is hit
+//Planned on 0 = empty, 1 = ship placed (un-hit), 2 = missed shot, 3 = hit ship, 4 = sunk ship
 //EX: [0,0,1,1,2,1,0]
 let gameData = {
 
@@ -16,10 +16,64 @@ let gameData = {
 
 }
 
+function gameplayLoop(gameData) {
+	let pos = 0;
+	while (gameData.isWon == false) {
+		if (gameData.currentPlayer == 1) {
+			//get fire information and store it in pos
+			switch (fire(gameData, pos) {
+				// copy/paste code below
+				case 0: //miss 
+				
+				break;
+				case 1: //hit
+				
+				break;
+				case 2: //sunk
+				
+				break;
+				default:
+				console.log("Something went real wrong.\n");
+			}
+		} else {
+			//get fire information and store it in pos
+			switch (fire(gameData, pos) {
+				case 0: //miss 
+				
+				break;
+				case 1: //hit
+				
+				break;
+				case 2: //sunk
+				
+				break;
+				default:
+				console.log("Something went real wrong.\n");
+			}
+		}
+		winCheck(gameData);
+		
+		//Switch current player
+		gameData.currentPlayer = (gameData.currentPlayer + 1)%2;
+	}
+}
 
-
-
-
+function fire(gameData, pos) {
+	
+	//Get value stored where shot was placed
+	if (gameData.currentPlayer == 1) {
+		let result = gameData.player1arr[pos];
+	} else {
+		let result = gameData.player2arr[pos];
+	}
+	
+	if (result == 1) { //only executes if un-hit ship is detected
+		gameData.player1arr[pos] = 3
+		//if ship is sunk, set gameData.player1arr[pos] = 4 and return 2
+		return(1);
+	}
+	return(0);		
+}
 
 
 /**
